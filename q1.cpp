61888543762d013334cd6ffbc70299224c3ed144
Simrik Pokharel <pokharelsimrik27@gmail.com>
2024-09-12 11:53:01 +0545

    Files

--- q1.cpp
+++ q1.cpp
@@ -5,19 +5,19 @@ using namespace std;
 bool isPerfectNumber (int num){
     int sum=0;
     //Finding all divisors of num and calculating their sum
-    for (int i=1; i<=num /2; i++){
+    for (int i=1; i<=num /2; ++i){
         if (num % i ==0){
             sum +=i;
         }
     }
-    // A number is perfect if sum of its divisoirs equals the number itself
+    // A number is perfect if sum of its divisors equals the number itself
     return sum == num;
 }
-int main (){
+int main(){
     cout << "Perfect numbers between 1 and 500 are:" <<endl;
 
     //Iterating through the numbers from 1 to 500 and checking for perfection
-    for (int i=1; i <= 500, i++){
+    for (int i=1; i <= 500; ++i){
         if (isPerfectNumber(i)){
             cout << i << " ";
         }
