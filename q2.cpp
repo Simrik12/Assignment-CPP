1c1ea3d15998e060fc15f2f2b70baf1be94c703e
Simrik Pokharel <pokharelsimrik27@gmail.com>
2024-09-12 14:15:38 +0545

    Files

--- q2.cpp
+++ q2.cpp
@@ -1,60 +1,28 @@
 #include <iostream>
 using namespace std;
 
-class Date {
-    private:
-    int day, month, year;
-
-public:
-//Function to set the date
-void setDate (int d, int m, int y){
-    day=d;
-    month=m;
-    year=y;
-}
-// Function to get and display the date
-void getDate (){
-    cout << "The date is: " << day << "/" << month << "/" << year <<endl;
-}
-//Function to validate the date
-bool isValid (){
-    if (year >=1 && month >=1 && month <=12){
-        if ((day >= 1 && day <=31) && (month ==1 || month ==3 || month ==5 || month ==7 || month ==8 || month ==10 || month ==12)){
-            return true; //Month with 31 days
-        } else if ((day >=1 && day <=30) && (month ==4 || month == 6 || month ==9 || month ==11 )) {
-            return true; //Month with 30 days
-        } else if (month ==2){
-            bool isLeap = (year %4 ==0 && (year % 100 !=0 || year % 400 ==0));
-            if (isLeap && day>=1 && day <=29){
-                return true; //Leap year (February)
-            } else if (!isLeap && day >=1 && day <=28){
-                return true; //Non-leap year (February)
-            }
-        }
+//Function to check if a number is prime or not
+bool isPrime (int num){
+    if (num <=1) return false;
+    for (int i=2; i <=num / 2; ++i){
+        if (num% i ==0) return false;
     }
-    return false;
-    }
-};
+    return true;
+}
 int main (){
-    Date date;
-    int d,m,y;
+    int number;
 
-    cout << "Enter day: ";
-    cin >>d;
-    cout << "Enter month: ";
-    cin >>m;
-    cout << "Enter year: ";
-    cin >>y;
+    cout << "Enter a number: ";
+    cin  >> number;
 
-    //Setting the date
-    date.setDate (d,m,y);
-
-    //Validating and display the date
-    if (date.isValid()){
-        date.getDate();
-        cout << "The date is valid." << endl;
-    } else {
-        cout << "The date is invalid." << endl;
-    }
-    return 0;
+    //Searching for the last prime number before the entered number
+    number --;  //Decrease the number by 1 to find the last prime number before it
+    while (number >1){
+        if (isPrime (number)){
+            cout << "The last prime number before the entered number is: " << number << endl;
+            break;
+        }
+        number --; // Decrementing to find the next prime
+}
+return 0;
 }
