8ab0152860ef5e9f324b43895a4fe23c3f025d1d
Simrik Pokharel <pokharelsimrik27@gmail.com>
2024-09-14 13:10:40 +0545

    Files

--- q1.cpp
+++ q1.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 using namespace std;
 
-//Function to check if the number is perfect
-bool isPerfectNumber (int num){
-    int sum=0;
-    //Finding all divisors of num and calculating their sum
-    for (int i=1; i<=num /2; ++i){
-        if (num % i ==0){
-            sum +=i;
-        }
+//Function to calculate the sum of elements in the array
+int sumArray (int arr[], int size){
+//Base Case: If array size is 0 then return 0
+    if (size <= 0){
+        return 0;
     }
-    // A number is perfect if sum of its divisors equals the number itself
-    return sum == num;
+// Recursive case: Adding the last element to sum the rest of the array
+   return arr [size- 1] + sumArray (arr, size- 1);
 }
-int main(){
-    cout << "Perfect numbers between 1 and 500 are:" <<endl;
 
-    //Iterating through the numbers from 1 to 500 and checking for perfection
-    for (int i=1; i <= 500; ++i){
-        if (isPerfectNumber(i)){
-            cout << i << " ";
-        }
-    }
-    cout <<endl;
-    return 0;
+int main (){
+    int n;
+
+cout << "Enter the number of elements: ";
+cin >> n;
+
+int arr [n];
+cout << "Enter all the elements: " << endl;
+for (int i=0; i < n; ++i){
+    cin >> arr[i];
+}
+//Calculating the sum of all elements using recursion
+int sum = sumArray (arr, n);
+
+cout << "The sum of all elements is: " << sum << endl;
+
+return 0;
 }
