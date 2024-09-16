d137d76435d1091167b7ba96fd157c13b252958c
Simrik Pokharel <pokharelsimrik27@gmail.com>
2024-09-16 16:24:10 +0545

    Files

--- q1.cpp
+++ q1.cpp
@@ -1,31 +1,20 @@
-#include <iostream>
+ #include <iostream>
 using namespace std;
 
-//Function to calculate the sum of elements in the array
-int sumArray (int arr[], int size){
-//Base Case: If array size is 0 then return 0
-    if (size <= 0){
-        return 0;
+int main() {
+    int rows;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    // Outer loop for the number of rows
+    for(int i = 1; i <= rows; ++i) {
+        // Inner loop for printing asterisks
+        for(int j = 1; j <= i; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
     }
-// Recursive case: Adding the last element to sum the rest of the array
-   return arr [size- 1] + sumArray (arr, size- 1);
-}
 
-int main (){
-    int n;
-
-cout << "Enter the number of elements: ";
-cin >> n;
-
-int arr [n];
-cout << "Enter all the elements: " << endl;
-for (int i=0; i < n; ++i){
-    cin >> arr[i];
-}
-//Calculating the sum of all elements using recursion
-int sum = sumArray (arr, n);
-
-cout << "The sum of all elements is: " << sum << endl;
-
-return 0;
+    return 0;
 }
