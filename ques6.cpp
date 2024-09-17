242e9668080e942db25788c4aaabbb12273b460b
Simrik Pokharel <pokharelsimrik27@gmail.com>
2024-09-17 21:08:39 +0545

    Files

--- ques6.cpp
+++ ques6.cpp
@@ -1,24 +1,50 @@
 #include <iostream>
+#include <algorithm>   //For sorting
 using namespace std;
 
 int main () {
-    int rows;
+    int n;
 
-    cout << "Enter the number of rows: ";
-    cin >> rows;
+    cout << "Enter the number of elements in the array (at least 4 elements): ";
+    cin >> n;
 
-    //Looping through each row
-    for (int i = 1; i <= rows; ++i) {
-        //Looping through each colum in the current row
-        for (int j = 1; j <= i; ++j) {
-            //Printing 1 if the sum of row and colum indices is even, if not then print 0
-            if ((i + j) % 2 ==0){
-                cout << "1";
-            } else {
-                cout << "0";
-            }
-        }
-        cout << endl;
+    //Checking if the input size is large enough
+    if (n < 4) {
+        cout << "The array must have at least 4 elements."  << endl;
+        return 1;
     }
-    return 0;
+    
+    int arr[n];
+
+    cout << "Enter " << n << " elements:"  << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    //Sorting the array in ascending order
+    sort (arr, arr + n);
+    cout << "The repeating elements are: ";
+
+    int count = 0;  //Counter for the number of different repeating elements
+
+    //Finding and displaying upto two distinct repeating elements
+    for (int i =1; i < n; i++) {
+        //Checking if the current element is the same as the previous element
+    if (arr [i] == arr [i-1]){
+        //Printing the element only if it is the first occurence or the last occurence of this element
+        if (count == 0 || arr [i] != arr [i-2]) {
+            cout << arr[i] << " ";
+            count ++;
+        }
+        //Stop if two distinct repeating elements are found
+        if (count ==2) break;
+    }
+    }
+    if (count == 0) {
+        cout << "None";   //Message if no repeating elements are foun
+}
+
+cout << endl;
+            
+return 0;
 }
