a0b9ede6d1a3b0a790e9879d0876561010e0cec0
Simrik Pokharel <pokharelsimrik27@gmail.com>
2024-09-17 18:37:13 +0545

    Files

--- q1.cpp
+++ q1.cpp
@@ -1,20 +1,33 @@
  #include <iostream>
+ #include <algorithm>  //For sorting
 using namespace std;
 
-int main() {
-    int rows;
+int main(){
+    int n;
 
-    cout << "Enter the number of rows: ";
-    cin >> rows;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
 
-    // Outer loop for the number of rows
-    for(int i = 1; i <= rows; ++i) {
-        // Inner loop for printing asterisks
-        for(int j = 1; j <= i; ++j) {
-            cout << "* ";
-        }
-        cout << endl;
-    }
-
-    return 0;
+    //Checking if the input size is large enough
+    if (n < 3) {
+        cout << "The array must have at least 3 elements." << endl;
+        return 1;
 }
+
+int arr[n];
+
+//Inputting the elements of array
+cout << "Enter" << n << "elements:" <<endl;
+for (int i = 0; i < n; i++) {
+    cin >> arr [i];
+}
+
+//Sorting the array in ascending order
+sort(arr, arr + n);
+
+cout << "The largest three elements are: "
+    << arr [n-1] << ", " << arr[n-2] << ", and " << arr [n-3]  << endl;
+
+return 0;
+}
+
