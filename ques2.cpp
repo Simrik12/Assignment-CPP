1983c984278ea55a02d32483db91cd8cd6026fc2
Simrik Pokharel <pokharelsimrik27@gmail.com>
2024-09-17 19:18:17 +0545

    Files

---
+++ ques2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>       //For sorting
+using namespace std;
+
+int main () {
+    int n;
+
+cout << "Enter the number of elements in the array: ";
+cin >> n;
+
+//Checking if the input size is large enough
+if (n < 2) {
+    cout << "The array must have at least 2 elements."  << endl;
+    return 1;
+}
+
+int arr[n];
+cout << "Enter "  << n << " elements:"  << endl;
+for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+}
+
+//Sorting the array in ascending order
+sort (arr, arr + n);
+
+cout << "The second largest element is: " << arr [n-2] << endl;
+
+return 0;
+}
