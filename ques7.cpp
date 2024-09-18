cfa87e96b138e9e8b825164fc8f8e7973315da9b
Simrik Pokharel <pokharelsimrik27@gmail.com>
2024-09-18 14:57:29 +0545

    Files

--- ques7.cpp
+++ ques7.cpp
@@ -1,38 +1,39 @@
 #include <iostream>
-#include <algorithm>    //For sorting
+#include <unordered_set>  // For storing the seen elements
+
 using namespace std;
 
-int main () {
+int main() {
     int n;
-
+    
     cout << "Enter the number of elements in the array: ";
     cin >> n;
 
-    //Checking if the input size is large enough
-    if (n < 2) {
-        cout << "The array must have at least 2 elements."   << endl;
+    // Checking if the input size is large enough
+    if(n < 2) {
+        cout << "Array must have at least 2 elements." << endl;
         return 1;
     }
-
-    int arr [n];
-    cout << "Enter " << n << "elements:"  << endl;
-    for (int i = 0; i < n; i ++) {
-        cin >> arr [i];
+    
+    int arr[n];
+    cout << "Enter " << n << " elements:" << endl;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
+    
+    unordered_set<int> seen;  // Setting to track elements we have encountered
 
-    //Sorting the array in ascending order
-    sort (arr, arr + n);
-
-    cout << "The firest repeating element is: ";
-
-    //Finding and displaying the first repeating element
-    for (int i = 1; i < n; i++) {
-        if (arr[i] == arr [i-1]) {
-            cout << arr [i] << endl;
-            return 0;        //Exiting after finding the first repeating element
-      }
+    // Finding the first repeating element without sorting
+    for(int i = 0; i < n; i++) {
+        if(seen.find(arr[i]) != seen.end()) {
+            cout << "The first repeating element is: " << arr[i] << endl;
+            return 0;  // Exiting after finding the first repeating element
+        }
+        seen.insert(arr[i]);  // Adding the current element to the set
     }
-    cout << "None" << endl;   //Message if no repeating elements are found
-
+    
+    cout << "The first repeating element is none" << endl;  // Message if no repeating elements are found
+    
     return 0;
-} 
+}
+
