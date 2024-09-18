e76b72c38b1b5881d49ec3605982db71af004e86
Simrik Pokharel <pokharelsimrik27@gmail.com>
2024-09-18 14:36:49 +0545

    Files

--- ques7.cpp
+++ ques7.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
+#include <algorithm>    //For sorting
 using namespace std;
 
-int main (){
-    int rows;
+int main () {
+    int n;
 
-    cout << "Enter the number of rows: ";
-    cin >> rows;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
 
-    //Looping through each row
-    for (int i = 0; i < rows; ++i) {
-        int number =1;
+    //Checking if the input size is large enough
+    if (n < 2) {
+        cout << "The array must have at least 2 elements."   << endl;
+        return 1;
+    }
 
-    //Looping through each column in the current row
-    for (int j = 0; j <=i; ++j){
-        cout << number <<  " ";
-        number = number * (i - j) / (j + 1);   //Computing the next number in the row
-}
+    int arr [n];
+    cout << "Enter " << n << "elements:"  << endl;
+    for (int i = 0; i < n; i ++) {
+        cin >> arr [i];
+    }
 
-cout << endl;
-}
+    //Sorting the array in ascending order
+    sort (arr, arr + n);
+
+    cout << "The firest repeating element is: ";
+
+    //Finding and displaying the first repeating element
+    for (int i = 1; i < n; i++) {
+        if (arr[i] == arr [i-1]) {
+            cout << arr [i] << endl;
+            return 0;        //Exiting after finding the first repeating element
+      }
+    }
+    cout << "None" << endl;   //Message if no repeating elements are found
+
     return 0;
-}
+} 
