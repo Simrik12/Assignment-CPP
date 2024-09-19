348c67201e3164ef3af6570a85e4b84cfb397c73
Simrik Pokharel <pokharelsimrik27@gmail.com>
2024-09-19 12:16:22 +0545

    Files

--- ques7.cpp
+++ ques7.cpp
@@ -1,39 +1,27 @@
 #include <iostream>
-#include <unordered_set>  // For storing the seen elements
-
+#include <string>
 using namespace std;
 
-int main() {
-    int n;
-    
-    cout << "Enter the number of elements in the array: ";
-    cin >> n;
+int main () {
+    string s;
+    char c;
 
-    // Checking if the input size is large enough
-    if(n < 2) {
-        cout << "The array must have at least 2 elements." << endl;
-        return 1;
-    }
-    
-    int arr[n];
-    cout << "Enter " << n << " elements:" << endl;
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    
-    unordered_set<int> seen;  // Setting to track elements we have encountered
+//Asking the user for the string and character
+cout << "Enter the string: ";
+cin >> s;
+cout << "Enter the character to remove: ";
+cin >> c;
 
-    // Finding the first repeating element without sorting
-    for(int i = 0; i < n; i++) {
-        if(seen.find(arr[i]) != seen.end()) {
-            cout << "The first repeating element is: " << arr[i] << endl;
-            return 0;  // Exiting after finding the first repeating element
-        }
-        seen.insert(arr[i]);  // Adding the current element to the set
+//String to store the updated version
+string result;
+
+//Looping through each character and adding to result if not equal to c
+for (char ch : s) {
+    if (ch != c) {
+        result += ch;
     }
-    
-    cout << "No repeating elements found." << endl;  // Message if no repeating elements are found
-    
-    return 0;
 }
+cout << "The updated string is: "  << result << endl;
 
+return 0;
+}
