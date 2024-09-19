3081ee0c1197e7bbbed8b9b228da1bd16560bc2d
Simrik Pokharel <pokharelsimrik27@gmail.com>
2024-09-19 14:47:06 +0545

    Files

--- ques4.cpp
+++ ques4.cpp
@@ -4,33 +4,35 @@
 
 using namespace std;
 
-// Function to check if all letters in str2 are in str1
-bool areLettersInString1(const string &str1, const string &str2) {
-    for (char c : str2) {
-        if (find(str1.begin(), str1.end(), c) == str1.end()) {
-            return false; // If the letter is not found in str1
+// Function to check if all letters in string2 are in string1
+bool areLettersInString1(const string &string1, const string &string2) {
+    for (char c : string2) {
+        if (find(string1.begin(), string1.end(), c) == string1.end()) {
+            return false; // If letter is not found in str1
         }
     }
-    return true; // If the letters is found in str1
+    return true; // If all letters are found in str1
 }
 
 int main() {
-    string str1, str2;
+    string string1, string2;
     cout << "Enter the first string: ";
-    cin >> str1;
+    cin >> string1;
     cout << "Enter the second string: ";
-    cin >> str2;
+    cin >> string2;
     
     // Checking if all letters in str2 are in str1
-    bool result = areLettersInString1(str1, str2);
+    bool result = areLettersInString1(string1, string2);
     
+    cout << "The original string elements are: " << string1 << " " << string2 << endl;
     if (result) {
-        cout << "All letters in the second string are in the first string." << endl;
+        cout << "The first string does contain all the letters from the second string." << endl;
     } else {
-        cout << "Not all letters in the second string are in the first string." << endl;
+        cout << "The first string doesn't contain all letters from the second string." << endl;
     }
     
     return 0;
 }
+
 
 
